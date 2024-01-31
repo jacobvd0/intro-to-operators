775f98b51a8a6fa2993064ee8963b81fe0954fc8
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-01-31 04:06:51 +0000

    done but need to make float instead of int

--- intro to operators.cpp
+++ intro to operators.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     int first = 0;
+    int second = 0;
 
     std::cout << "Enter the first number: ";
     std::cin >> first;
@@ -15,5 +16,25 @@ int main()
         std::cin.ignore(std::cin.rdbuf()->in_avail());
     }
 
-    
+
+    std::cout << "Enter the second number: ";
+    std::cin >> second;
+    std::cout << "\n";
+
+    if (std::cin.fail()) {
+        std::cout << "You didn't enter an integer. \nDefaulting to using 5.\n";
+        second = 5;
+        std::cin.clear();
+        std::cin.ignore(std::cin.rdbuf()->in_avail());
+    }
+
+    std::cout << "The 2 numbers entered are " << first << " and " << second << "\n";
+    std::cout << first << " + " << second << " = " << (first + second) << "\n";
+    std::cout << first << " - " << second << " = " << (first - second) << "\n";
+    std::cout << first << " / " << second << " = " << (first / second) << "\n";
+    std::cout << first << " * " << second << " = " << (first * second) << "\n";
+    std::cout << first << " % " << second << " = " << (first % second) << "\n";
+
+    std::cin.ignore(std::cin.rdbuf()->in_avail());
+    std::cin.get();
 }
